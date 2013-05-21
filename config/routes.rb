fb52aebdcbb114e327fd1df180be877c80dd25b1
lionhuang <lion.4757@gmail.com>
2013-05-21 22:16:52 +0800

    bootstrap 設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Lionsruby::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
